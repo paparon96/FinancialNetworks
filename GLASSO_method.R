9bdcc9b4ea46f8395bc0eb97a1841e1950f91927
Aron <aron.pap8@gmail.com>
2020-11-15 14:11:00 +0100

    Created new visualisation and centrality comparison notebooks

--- GLASSO_method.R
+++ GLASSO_method.R
@@ -8,25 +8,65 @@ library(corpcor)
 # Import custom functions
 source("common_functions.R")
 
-# Import data
-df <- read.table('./Data/Stock_prices/log_returns_all_ts.csv',sep=",", header=TRUE)
-
 ## Global parameters
+method = "GLASSO"
+type = "volatility"
 var_cols = c("MS","JPM","BAC","C","WFC","GS","USB","TD","BK","TFC")
-N = dim(stock_df)[1]
 window_length = 150
 final_date = as.Date("2020-06-30")
-final_date_loc = match(final_date,as.Date(df$Date))
-start_date_loc = final_date_loc - window_length #final_date - window_length
+rho_hyperparam = 0.25
+
+###### Import data
+
+# Returns
+return_df <- read.table('./Data/Stock_prices/log_returns_all_ts.csv',sep=",", header=TRUE)
+
+# Format date column
+return_df$Date <- as.Date( format( as.Date( return_df$Date, format = "%d/%m/%Y" ), "%Y-%m-%d"))
+
+# Volatilities #############
+vol <- list()
+vol$data <- list()
+vol$data$high_low <- cbind( as.data.frame( read.csv( "Data/Stock_prices/high_price.csv", header = TRUE, sep = "," ) ),
+                            as.data.frame( read.csv( "Data/Stock_prices/low_price.csv", header = TRUE, sep = "," ) )[ , -1 ] )
+colnames( vol$data$high_low )[ 1 ] <- "Date"
+vol$data$high_low$Date <- as.Date( format( as.Date( vol$data$high_low$Date, format = "%d/%m/%Y" ), "%Y-%m-%d"))
+risk_factors_5 <- as.data.frame( read.csv( "Data/Stock_prices/F-F_Research_Data_5_Factors_2x3_daily.CSV", header = TRUE, sep = "," ))
+
+# Calculating the annualized daily percent standard deviation
+vol$data$volatility <- as.data.frame( matrix( nrow = nrow( vol$data$high_low ), ncol = ncol( vol$data$high_low[ , -1 ] ) / 2 + 1 ) )
+vol$data$volatility[ , 1 ] <- vol$data$high_low$Date
+for( i in 1:10 ){
+  vol$data$volatility[ , i + 1 ] <- log( 100 * sqrt( 365 * 0.361 * ( log( vol$data$high_low[ , i + 1 ] ) -  log( vol$data$high_low[ , i + 11 ] ) )^2 ) )
+}
+
+colnames( vol$data$volatility ) <- colnames( vol$data$high_low )[ 1:11 ]
+
+volatility_df = vol$data$volatility
+#################
+
+# Get modelling dataframe based on the current modelling type
+if (type=="return"){
+  
+  df = return_df
+} else if (type=="volatility"){
+  
+  df = volatility_df
+} else{
+  print("Invalid modelling type defined, choose from 'return' or 'volatility'!")
+  
+}
 
 # Filter dataset based on the dates
-#filtered_df = subset(df,as.Date(Date) >= start_date)
-#filtered_df = subset(filtered_df, final_date as.Date(Date))
-filtered_df = df[start_date_loc:(final_date_loc-1),]
-filtered_df = filtered_df[var_cols]
+filtered_df = subset(df, final_date >= as.Date(Date))
+start_date = tail(filtered_df$Date,n=window_length)[1]
+filtered_df = subset(filtered_df,as.Date(Date) >= start_date)
 
 # Inspect the results
 plot(filtered_df$MS)
+
+# Get only the relevant columns
+filtered_df = filtered_df[var_cols]
 
 # Standardize final dataset
 filtered_df <- scale(filtered_df)
@@ -43,51 +83,62 @@ filtered_df <- scale(filtered_df)
 
 # Get the sample covariance matrix of the data
 s <- var(filtered_df)
-rho_param <- .01
+rho_param <- rho_hyperparam
 
 # Run the algorithm
 result3 <-glasso(s, rho=rho_param)
 
 # Obtain the estimated covariance matrix
-estim_covmat = result3$w
+estim_covmat = result3$wi
 
 ## Transform the covariance matrix into a partial correlation matrix
-estimated_partial_corr_matrix = cor2pcor(estim_covmat)
+estimated_partial_corr_matrix = invcov2pcorr(estim_covmat)
 
 print(estimated_partial_corr_matrix)
 
 # Hyperparameter tuning - penalty terms
 
 ## Export results
-write.csv(estimated_partial_corr_matrix,'./Data/Estimated_networks/GLASSO.csv')
+#write.csv(estimated_partial_corr_matrix,'./Data/Estimated_networks/GLASSO.csv')
+# Name the file properly first
+final_date_transformed = paste0(substr(final_date,1,4),"_",
+                                substr(final_date,6,7),"_",
+                                substr(final_date,9,10))
+filename = paste0("./Data/Estimated_networks/",method,"_",type,"_",final_date_transformed,".csv")
+write.csv(estimated_partial_corr_matrix,filename)
 
 
 ## METHOD PART 2: Run combined model
   
-  # Import factors
-  five_factors_df <- read.table('./Data/Stock_prices/F-F_Research_Data_5_Factors_2x3_daily.CSV',sep=",", header=TRUE)
+# Import factors
+five_factors_df <- read.table('./Data/Stock_prices/F-F_Research_Data_5_Factors_2x3_daily.CSV',sep=",", header=TRUE)
 
-  ## parameters
-  var_cols = c("Mkt.RF","SMB","HML","RMW","CMA","RF")
-window_length = 150
-final_date = "30/06/2020" 
-final_date_loc = match(final_date,five_factors_df$Date)
-start_date_loc = final_date_loc - window_length #final_date - window_length
+# Format date column
+five_factors_df$Date <- as.Date( format( as.Date( five_factors_df$Date, format = "%d/%m/%Y" ), "%Y-%m-%d"))
 
-  filtered_factor_df = five_factors_df[start_date_loc:(final_date_loc-1),]
-filtered_factor_df = filtered_factor_df[var_cols]
+## parameters
+factor_var_cols = c("Mkt.RF","SMB","HML","RMW","CMA","RF")
 
-  # Inspect the results
-  plot(filtered_factor_df$SMB)
+# Filter dataset based on the dates
+filtered_factor_df = subset(five_factors_df, final_date >= as.Date(Date))
+#start_date = tail(filtered_factor_df$Date,n=window_length)[1]
+filtered_factor_df = subset(filtered_factor_df,as.Date(Date) >= start_date)
+
+# Get only the relevant columns
+filtered_factor_df = filtered_factor_df[factor_var_cols]
+
+# Inspect the results
+plot(filtered_factor_df$SMB)
+
 # Standardize final dataset
-  filtered_factor_df <- scale(filtered_factor_df)
+filtered_factor_df <- scale(filtered_factor_df)
 
-  # Combine arrays
-  combined_filtered_df <- cbind(filtered_df,filtered_factor_df)
+# Combine arrays
+combined_filtered_df <- cbind(filtered_df,filtered_factor_df)
 
   # Get the sample covariance matrix of the data
   s <- var(combined_filtered_df)
-rho_param <- 0.85 #0.01 #0.85
+rho_param <- rho_hyperparam #0.01 #0.85
 
   # Run the algorithm
   result3 <-glasso(s, rho=rho_param)
@@ -104,7 +155,8 @@ rho_param <- 0.85 #0.01 #0.85
   # Hyperparameter tuning - penalty terms
   
   ## Export results
-  write.csv(estimated_partial_corr_matrix,'./Data/Estimated_networks/GLASSO_combined_factors.csv')
+  filename = paste0("./Data/Estimated_networks/",method,"_",type,"_combined_factors_",final_date_transformed,".csv")
+  write.csv(estimated_partial_corr_matrix,filename)
   
   
   ## METHOD PART 3: Model with residuals from factor regression
@@ -115,7 +167,7 @@ factor_residual_matrix <- get_residuals(filtered_df,filtered_factor_df)
 factor_residual_matrix <- scale(factor_residual_matrix)
 # Get the sample covariance matrix of the data
   s <- var(factor_residual_matrix)
-rho_param <- 0.8
+rho_param <- rho_hyperparam
 
   # Run the algorithm
   result3 <-glasso(s, rho=rho_param)
@@ -130,6 +182,7 @@ rho_param <- 0.8
 
 
   # Hyperparameter tuning - penalty terms
-  write.csv(estimated_partial_corr_matrix,'./Data/Estimated_networks/GLASSO_factor_residuals.csv')
+  filename = paste0("./Data/Estimated_networks/",method,"_",type,"_factor_resid_",final_date_transformed,".csv")
+  write.csv(estimated_partial_corr_matrix,filename)
 
 
--- SPACE_method.R
+++ SPACE_method.R
@@ -7,31 +7,70 @@ library(ggplot2)
 # Import custom functions
 source("common_functions.R")
 
-# Import data
-df <- read.table('./Data/Stock_prices/log_returns_all_ts.csv',sep=",", header=TRUE)
-
-
-
 ## Global parameters
+method = "SPACE"
+type = "volatility"
 var_cols = c("MS","JPM","BAC","C","WFC","GS","USB","TD","BK","TFC")
-N = dim(stock_df)[1]
 window_length = 150
-final_date = as.Date("2020-06-30")
-final_date_loc = match(final_date,as.Date(df$Date))
-start_date_loc = final_date_loc - window_length #final_date - window_length
+final_date = as.Date("2020-03-05")
+
+###### Import data
+
+# Returns
+return_df <- read.table('./Data/Stock_prices/log_returns_all_ts.csv',sep=",", header=TRUE)
+
+# Format date column
+return_df$Date <- as.Date( format( as.Date( return_df$Date, format = "%d/%m/%Y" ), "%Y-%m-%d"))
+
+# Volatilities #############
+vol <- list()
+vol$data <- list()
+vol$data$high_low <- cbind( as.data.frame( read.csv( "Data/Stock_prices/high_price.csv", header = TRUE, sep = "," ) ),
+                            as.data.frame( read.csv( "Data/Stock_prices/low_price.csv", header = TRUE, sep = "," ) )[ , -1 ] )
+colnames( vol$data$high_low )[ 1 ] <- "Date"
+vol$data$high_low$Date <- as.Date( format( as.Date( vol$data$high_low$Date, format = "%d/%m/%Y" ), "%Y-%m-%d"))
+risk_factors_5 <- as.data.frame( read.csv( "Data/Stock_prices/F-F_Research_Data_5_Factors_2x3_daily.CSV", header = TRUE, sep = "," ))
+
+# Calculating the annualized daily percent standard deviation
+vol$data$volatility <- as.data.frame( matrix( nrow = nrow( vol$data$high_low ), ncol = ncol( vol$data$high_low[ , -1 ] ) / 2 + 1 ) )
+vol$data$volatility[ , 1 ] <- vol$data$high_low$Date
+for( i in 1:10 ){
+  vol$data$volatility[ , i + 1 ] <- log( 100 * sqrt( 365 * 0.361 * ( log( vol$data$high_low[ , i + 1 ] ) -  log( vol$data$high_low[ , i + 11 ] ) )^2 ) )
+}
+
+colnames( vol$data$volatility ) <- colnames( vol$data$high_low )[ 1:11 ]
+
+volatility_df = vol$data$volatility
+#################
+
+# Get modelling dataframe based on the current modelling type
+if (type=="return"){
+  
+  df = return_df
+} else if (type=="volatility"){
+  
+  df = volatility_df
+} else{
+  print("Invalid modelling type defined, choose from 'return' or 'volatility'!")
+  
+}
 
 # Filter dataset based on the dates
-#filtered_df = subset(df,as.Date(Date) >= start_date)
-#filtered_df = subset(filtered_df, final_date > as.Date(Date))
-filtered_df = df[start_date_loc:(final_date_loc-1),]
-filtered_df = filtered_df[var_cols]
+filtered_df = subset(df, final_date >= as.Date(Date))
+start_date = tail(filtered_df$Date,n=window_length)[1]
+filtered_df = subset(filtered_df,as.Date(Date) >= start_date)
 
 # Inspect the results
 plot(filtered_df$MS)
 
+# Get only the relevant columns
+filtered_df = filtered_df[var_cols]
+
 # Standardize final dataset
 filtered_df <- scale(filtered_df)
 
+
+## METHOD 1: Base case (without factors)
 
 ## Run the model
 
@@ -53,6 +92,83 @@ estimated_partial_corr_matrix = result2$ParCor
 # Hyperparameter tuning - penalty terms
 
 ## Export results
-write.csv(estimated_partial_corr_matrix,'./Data/Estimated_networks/SPACE.csv')
+# Name the file properly first
+final_date_transformed = paste0(substr(final_date,1,4),"_",
+                                substr(final_date,6,7),"_",
+                                substr(final_date,9,10))
+filename = paste0("./Data/Estimated_networks/",method,"_",type,"_",final_date_transformed,".csv")
+write.csv(estimated_partial_corr_matrix,filename)
 
+
+
+## METHOD PART 2: Run combined model
+
+# Import factors
+five_factors_df <- read.table('./Data/Stock_prices/F-F_Research_Data_5_Factors_2x3_daily.CSV',sep=",", header=TRUE)
+
+# Format date column
+five_factors_df$Date <- as.Date( format( as.Date( five_factors_df$Date, format = "%d/%m/%Y" ), "%Y-%m-%d"))
+
+## parameters
+factor_var_cols = c("Mkt.RF","SMB","HML","RMW","CMA","RF")
+
+# Filter dataset based on the dates
+filtered_factor_df = subset(five_factors_df, final_date >= as.Date(Date))
+#start_date = tail(filtered_factor_df$Date,n=window_length)[1]
+filtered_factor_df = subset(filtered_factor_df,as.Date(Date) >= start_date)
+
+# Get only the relevant columns
+filtered_factor_df = filtered_factor_df[factor_var_cols]
+
+# Inspect the results
+plot(filtered_factor_df$SMB)
+
+# Standardize final dataset
+filtered_factor_df <- scale(filtered_factor_df)
+
+# Combine arrays
+combined_filtered_df <- cbind(filtered_df,filtered_factor_df)
+
+## Run the model
+# Parameters
+n=nrow(combined_filtered_df)
+p=ncol(combined_filtered_df)
+alpha=1
+l1=(1/sqrt(n)*qnorm(1-alpha/(2*p^2)))*0.7
+iter=3
+
+result2=space.joint(data.matrix(combined_filtered_df), lam1=l1*n*1.56, lam2=0, iter=iter)
+print(result2)
+
+estimated_partial_corr_matrix = result2$ParCor
+
+## Export results
+filename = paste0("./Data/Estimated_networks/",method,"_",type,"_combined_factors_",final_date_transformed,".csv")
+write.csv(estimated_partial_corr_matrix,filename)
+
+
+## METHOD PART 3: Model with residuals from factor regression
+
+factor_residual_matrix <- get_residuals(filtered_df,filtered_factor_df)
+
+
+factor_residual_matrix <- scale(factor_residual_matrix)
+
+## Run the model
+# Parameters
+n=nrow(factor_residual_matrix)
+p=ncol(factor_residual_matrix)
+alpha=1
+l1=(1/sqrt(n)*qnorm(1-alpha/(2*p^2)))*0.7
+iter=3
+
+result2=space.joint(data.matrix(factor_residual_matrix), lam1=l1*n*1.56, lam2=0, iter=iter)
+print(result2)
+
+estimated_partial_corr_matrix = result2$ParCor
+
+
+## Export results
+filename = paste0("./Data/Estimated_networks/",method,"_",type,"_factor_resid_",final_date_transformed,".csv")
+write.csv(estimated_partial_corr_matrix,filename)
 

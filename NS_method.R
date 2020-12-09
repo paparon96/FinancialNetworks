03c4326fb145a1915fe67e46472ea920f0455f84
Aron <aron.pap8@gmail.com>
2020-12-09 20:55:02 +0100

    Rerun some stuff for 2020-03-16

--- NS_method.R
+++ NS_method.R
@@ -9,7 +9,7 @@ source("common_functions.R")
 
 ## Global parameters
 method = "NS"
-type = "volatility"
+type = "return"
 var_cols = c("MS","JPM","BAC","C","WFC","GS","USB","TD","BK","TFC")
 window_length = 150
 final_date = as.Date("2020-03-16")
